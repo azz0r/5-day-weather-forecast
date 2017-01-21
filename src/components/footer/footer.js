b7a425adadec973a603cbebf4b68f0553509a280
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-21 14:02:15 +0000

    Footer alt link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,6 +7,9 @@ export const Footer = () => {
       <p>Created by Aaron Lote as part of a technical test for BuildIt.</p>
       <p>If the page does not show any content please click the Shield in the address bar (Chrome) and click "Load unsafe scripts".</p>
       <p>This is due to Github Pages only supporting HTTPS while Openweathermap.com free requests only supporting HTTP.</p>
+      <p>
+        Alternately you can view it by <a href="http://5-day-weather-forecast.aaronlote.com" target="_blank">clicking here</a>
+      </p>
     </footer>
   )
 }
