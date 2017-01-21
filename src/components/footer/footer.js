d46ed9a8e7ab2c68949ac27a2d30e19d54de85ec
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-21 15:29:53 +0000

    Final footer tweaks

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,10 +5,9 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <p>Created by Aaron Lote as part of a technical test for BuildIt.</p>
-      <p>If the page does not show any content please click the Shield in the address bar (Chrome) and click "Load unsafe scripts".</p>
-      <p>This is due to Github Pages only supporting HTTPS while Openweathermap.com free requests only supporting HTTP.</p>
       <p>
-        Alternately you can view it by <a href="http://5-day-weather-forecast.aaronlote.com" target="_blank">clicking here</a>
+        If the page does not show any weather please try viewing it by <a href="http://5-day-weather-forecast.aaronlote.com" target="_blank">clicking here</a>
+        <strong> or</strong> clicking the shield in the address bar and pressing "load unsafe scripts"
       </p>
     </footer>
   )
